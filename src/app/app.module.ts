a82144124d74804e0f401c459ffb2e1297d505ea
edwarvilchez <edwarvilchez1977@gmail.com>
2020-09-06 19:59:58 -0400

    Creacion de modulos y componentes biometrics, customers, devolutions, products, header, footer, sidebar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DefaultModule } from './layouts/default/default.module'
+import { DefaultModule } from './layouts/default/default.module';
+
 
 
 
 @NgModule({
   declarations: [
     AppComponent
+
   ],
   imports: [
     BrowserModule,
--- src/app/layouts/default/default.module.ts
+++ src/app/layouts/default/default.module.ts
@@ -4,15 +4,29 @@ import { DefaultComponent } from './default.component';
 import { DashboardComponent } from '../../modules/dashboard/dashboard.component';
 import { RouterModule } from '@angular/router';
 
+// modulo importado
+import { SharedModule } from 'src/app/shared/shared.module';
+// componentes importados
+import { BiometricsComponent } from 'src/app/modules/biometrics/biometrics.component';
+import { CustomersComponent } from 'src/app/modules/customers/customers.component';
+import { DevolutionsComponent } from 'src/app/modules/devolutions/devolutions.component';
+import { ProductsComponent } from 'src/app/modules/products/products.component';
+
+
 
 @NgModule({
   declarations: [
     DefaultComponent,
-    DashboardComponent
+    DashboardComponent,
+    BiometricsComponent,
+    CustomersComponent,
+    DevolutionsComponent,
+    ProductsComponent
   ],
 
   imports: [
     CommonModule,
+    SharedModule,
     RouterModule
   ]
 })
