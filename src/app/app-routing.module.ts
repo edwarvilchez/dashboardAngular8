6e17e1548ce5621604f3aba3dbd6b155f0e56372
edwarvilchez <edwarvilchez1977@gmail.com>
2020-09-07 16:18:02 -0400

    Se crea la estructura del proyecto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,43 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DefaultComponent } from './layouts/default/default.component';
+import { DashboardComponent } from './modules/dashboard/dashboard.component';
+import { BiometricsComponent } from './modules/biometrics/biometrics.component';
+import { CustomersComponent } from './modules/customers/customers.component';
+import { DevolutionsComponent } from './modules/devolutions/devolutions.component';
+import { ProductsComponent } from './modules/products/products.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  // agregamos las rutas
+  {
+    path: '',
+    component: DefaultComponent,
+    children: [
+      {
+        path: '',
+        component: DashboardComponent
+      },
+      {
+        path: 'biometrics',
+        component: BiometricsComponent
+      },
+      {
+        path: 'customers',
+        component: CustomersComponent
+      },
+      {
+        path: 'devolutions',
+        component: DevolutionsComponent,
+      },
+      {
+        path: 'products',
+        component: ProductsComponent
+      }
+    ]
+  }
+// cierre de la declaración de las rutas
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/layouts/default/default.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DefaultComponent } from './default.component';
+import { DashboardComponent } from 'src/app/modules/dashboard/dashboard.component';
+import { RouterModule } from '@angular/router';
+import { BiometricsComponent } from 'src/app/modules/biometrics/biometrics.component';
+import { CustomersComponent } from 'src/app/modules/customers/customers.component';
+import { DevolutionsComponent } from 'src/app/modules/devolutions/devolutions.component';
+import { ProductsComponent } from 'src/app/modules/products/products.component';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { MatSidenavModule } from '@angular/material';
+
+
+
+@NgModule({
+  declarations: [
+    DefaultComponent,
+    DashboardComponent,
+    BiometricsComponent,
+    CustomersComponent,
+    DevolutionsComponent,
+    ProductsComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule,
+    SharedModule,
+    MatSidenavModule
+  ]
+})
+export class DefaultModule { }
